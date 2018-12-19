f9a73c8863de5248c579c3a7c0c7e4e3fcf1bf52
zhangcanming <635677619@qq.com>
2018-12-19 22:20:54 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("/helloController")
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello(){
+		return "index";
+	}
+
+
+}
